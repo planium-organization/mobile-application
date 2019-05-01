0b0b249135b6b4b723cefb5005279dca6a10f8c6
Amir Ehsandar <ehsandaramir@gmail.com>
2019-05-01 14:52:06 +0430

    fix bugs

--- src/screens/TimeTableScreen.js
+++ src/screens/TimeTableScreen.js
@@ -3,11 +3,8 @@ import { View, StyleSheet, Text, Button } from "react-native";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 
-import { View, StyleSheet , Text} from "react-native";
 import TimeTableEntity from "../Components/TimeTableEntity";
 import { ScrollView } from "react-native-gesture-handler";
-import { connect } from "react-redux";
-import { addCard, selectCard, deselectCard, deleteCard } from '../store/CardsActions';
 import {
   addCard,
   selectCard,
@@ -28,22 +25,33 @@ class TimeTableScreen extends Component {
         {/* Column Capital */}
         <View style={styles.ColumnCapital}>
           <View style={styles.ColumnCapitalBox}>
-            <Text style={{ textAlign: 'center', textAlignVertical:'center'}}>Yesterday</Text>
+            <Text style={{ textAlign: "center", textAlignVertical: "center" }}>
+              Yesterday
+            </Text>
           </View>
-          <View style={[styles.ColumnCapitalBox, { borderLeftWidth: 1, borderRightWidth: 1}]}>
-            <Text style={{ textAlign: 'center', textAlignVertical: 'center' }}>Today</Text>
+          <View
+            style={[
+              styles.ColumnCapitalBox,
+              { borderLeftWidth: 1, borderRightWidth: 1 }
+            ]}
+          >
+            <Text style={{ textAlign: "center", textAlignVertical: "center" }}>
+              Today
+            </Text>
           </View>
           <View style={styles.ColumnCapitalBox}>
-            <Text style={{ textAlign: 'center', textAlignVertical: 'center' }}>Tommorow</Text>
-          </View>  
+            <Text style={{ textAlign: "center", textAlignVertical: "center" }}>
+              Tommorow
+            </Text>
+          </View>
         </View>
 
         {/* Day columns */}
         <View style={{ flex: 1 }}>
-          <ScrollView style={{height: '100%'}}>
+          <ScrollView style={{ height: "100%" }}>
             <View style={styles.DayColumnScroll}>
               {/* columns */}
-              <View style={{ flex: 1, borderWidth: 1}}> 
+              <View style={{ flex: 1, borderWidth: 1 }}>
                 <Text>card1</Text>
               </View>
 
@@ -51,21 +59,12 @@ class TimeTableScreen extends Component {
                 <Text>card</Text>
               </View>
 
-              <View style={{ flex: 1, borderWidth:1 }}>
+              <View style={{ flex: 1, borderWidth: 1 }}>
                 <Text>card</Text>
-              </View>  
-              
+              </View>
             </View>
           </ScrollView>
         </View>
-//         <Text>{this.props.cards.length}</Text>
-//         <Button
-//           title="CLICK ME!"
-//           onPress={() => {
-//             this.addCard();
-//           }}
-//         />
-        {/* <TimeTableEntity styles={styles.timeTableEntity} /> */}
       </View>
     );
   }
@@ -79,8 +78,8 @@ const styles = StyleSheet.create({
     paddingTop: 10,
     paddingBottom: 10,
     flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
+    flexDirection: "column",
+    justifyContent: "center"
     // alignItems: 'stretch',
   },
   ColumnCapital: {
@@ -88,27 +87,26 @@ const styles = StyleSheet.create({
     paddingTop: 10,
     paddingBottom: 5,
     height: 70,
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "center"
   },
   DayColumnScroll: {
     height: 800,
     borderBottomWidth: 1,
     paddingTop: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'stretch',
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "stretch"
   },
-    main: {
-        flexDirection: 'column',
-        flex: 1,
-        // borderWidth: 1
-        //width: "100%"
-    },
-    timeTableEntity: {
-        // height: 60
-    }
-
+  main: {
+    flexDirection: "column",
+    flex: 1
+    // borderWidth: 1
+    //width: "100%"
+  },
+  timeTableEntity: {
+    // height: 60
+  }
 });
 
 const mapStateToProps = state => {
