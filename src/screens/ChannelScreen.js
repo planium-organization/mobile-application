c476120eb2747377804fdc0089956c11793b5b37
Amir Ehsandar <ehsandaramir@gmail.com>
2019-06-18 22:30:31 +0430

    primitive channel page design with post item definition

--- src/screens/ChannelScreen.js
+++ src/screens/ChannelScreen.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
-import { Button, View, Text } from "react-native";
+import { Button, View, Text, ScrollView } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
-import { ScrollView } from "react-native-gesture-handler";
+import ChannelPost from "./../Components/ChannelPost";
 
 class ChannelScreen extends Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <ScrollView style={{ flex: 1 }} >
+        <ScrollView style={{ flex: 1, width: "100%" }}>
           <View>
-            
+            <ChannelPost />
+            <ChannelPost />
+            <ChannelPost />
+            <ChannelPost />
           </View>
         </ScrollView>
       </View>
