d2049a2bc1f673e519c25cb705e13c8d1c5c9451
sobial <35309448+sobial@users.noreply.github.com>
2019-06-20 23:37:18 +0430

    Add analysis page and add chart

--- android/app/src/main/java/com/knownproject/MainApplication.java
+++ android/app/src/main/java/com/knownproject/MainApplication.java
@@ -3,11 +3,13 @@ package com.knownproject;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+//import com.horcrux.svg.SvgPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,7 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNGestureHandlerPackage()
+            new SvgPackage(),
+          new RNGestureHandlerPackage()
+
       );
     }
 
---
+++ src/screens/AnalysisScreen.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import { Button, View, Text, Dimensions } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import { ScrollView } from "react-native-gesture-handler";
+import {
+    LineChart,
+    BarChart,
+    PieChart,
+    ProgressChart,
+    ContributionGraph,
+    StackedBarChart
+} from 'react-native-chart-kit'
+const screenWidth = Dimensions.get('window').width
+
+class AnalysisScreen extends Component {
+
+    static navigationOptions = {
+        headerTitle: (
+            <View
+                style={{
+                    flex: 1,
+                    margin: 5,
+                    flexDirection: "row"
+                }}
+                >
+                <View style={{ width: 100 }}>
+                    <Button style={{}} title="Prev" onPress={() => { }} />
+                </View>
+                <Text
+                    style={{
+                        flex: 1,
+                        fontSize: 16,
+                        textAlignVertical: "center",
+                        textAlign: "center"
+                    }}
+                >
+                    Time Table
+                </Text>
+                <View style={{ width: 100 }}>
+                    <Button style={{}} title="Next" onPress={() => { }} />
+                </View>
+            </View>
+        )
+    };
+    render() {
+        const data = [
+            { name: 'Seoul', population: 21500000, color: 'rgba(131, 167, 234, 1)', legendFontColor: '#7F7F7F', legendFontSize: 15 },
+            { name: 'Toronto', population: 2800000, color: '#F00', legendFontColor: '#7F7F7F', legendFontSize: 15 },
+            { name: 'Beijing', population: 527612, color: 'red', legendFontColor: '#7F7F7F', legendFontSize: 15 },
+            { name: 'New York', population: 8538000, color: '#ffffff', legendFontColor: '#7F7F7F', legendFontSize: 15 },
+            { name: 'Moscow', population: 11920000, color: 'rgb(0, 0, 255)', legendFontColor: '#7F7F7F', legendFontSize: 15 }
+        ];
+        const chartConfig = {
+            backgroundGradientFrom: '#1E2923',
+            backgroundGradientTo: '#08130D',
+            color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+            //strokeWidth: 2 // optional, default 3
+        }
+        return (
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                <ScrollView style={{ flex: 2 , borderColor:"#000"}} >
+                    <View style={{paddingLeft:5,paddingRight:5,paddingTop:5,paddingBottom:5}}>
+                        <PieChart
+                            data={data}
+                            width={screenWidth-10}
+                            height={400}
+                            chartConfig={chartConfig}
+                            accessor="population"
+                            backgroundColor="transparent"
+                            //paddingLeft="15"
+                            doughnut={true}
+                            absolute
+                        />
+                       
+                    </View>
+                    <View>
+                        <Text>bar charts goes here</Text>
+                    </View>
+                </ScrollView>
+            </View>
+        );
+    }
+}
+
+export default AnalysisScreen;
